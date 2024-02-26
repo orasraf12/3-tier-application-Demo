edfae4add71ca88b740ade148858f1e0d4728a74
Or Asraf <orasraf@OrA-MacM3Pro.local>
2024-02-26 19:05:48 +0200

    first commit

---
+++ terraform/provider.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = var.region
+
+  default_tags {
+    tags = var.tags
+  }
+}
+
+
+terraform {
+  backend "s3" {
+    bucket = "or-asraf-demo"
+    region = var.region
+  }
+}
+
